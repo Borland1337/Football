fa103b250f27e4d4b33f26723f60fbc6ed38abd6
Borland1337 <32260078+Borland1337@users.noreply.github.com>
2024-01-29 23:58:58 +0500

    update to android

--- Assets/Photon/PhotonUnityNetworking/UtilityScripts/Prototyping/MoveByKeys.cs
+++ Assets/Photon/PhotonUnityNetworking/UtilityScripts/Prototyping/MoveByKeys.cs
@@ -45,7 +45,7 @@ namespace Photon.Pun.UtilityScripts
 
         private bool isSprite;
         private float jumpingTime;
-        private Rigidbody body;
+        //ivate Rigidbody body;
         private Rigidbody2D body2d;
 
         public void Start()
@@ -54,58 +54,59 @@ namespace Photon.Pun.UtilityScripts
             this.isSprite = (GetComponent<SpriteRenderer>() != null);
 
             this.body2d = GetComponent<Rigidbody2D>();
-            this.body = GetComponent<Rigidbody>();
+            //  this.body = GetComponent<Rigidbody>();
         }
 
 
         // Update is called once per frame
         public void FixedUpdate()
         {
-            if (!photonView.IsMine)
-            {
-                return;
-            }
+            //    if (!photonView.IsMine)
+            //    {
+            //        return;
+            //    }
 
-            if ((Input.GetAxisRaw("Horizontal") < -0.1f) || (Input.GetAxisRaw("Horizontal") > 0.1f))
-            {
-                transform.position += Vector3.right * (Speed * Time.deltaTime) * Input.GetAxisRaw("Horizontal");
-            }
+            //    if ((Input.GetAxisRaw("Horizontal") < -0.1f) || (Input.GetAxisRaw("Horizontal") > 0.1f))
+            //    {
+            //        transform.position += Vector3.right * (Speed * Time.deltaTime) * Input.GetAxisRaw("Horizontal");
+            //    }
 
-            // jumping has a simple "cooldown" time but you could also jump in the air
-            if (this.jumpingTime <= 0.0f)
-            {
-                if (this.body != null || this.body2d != null)
-                {
-                    // obj has a Rigidbody and can jump (AddForce)
-                    if (Input.GetKey(KeyCode.Space))
-                    {
-                        this.jumpingTime = this.JumpTimeout;
+            //    // jumping has a simple "cooldown" time but you could also jump in the air
+            //    if (this.jumpingTime <= 0.0f)
+            //    {
+            //        if (this.body != null || this.body2d != null)
+            //        {
+            //            // obj has a Rigidbody and can jump (AddForce)
+            //            if (Input.GetKey(KeyCode.Space))
+            //            {
+            //                this.jumpingTime = this.JumpTimeout;
 
-                        Vector2 jump = Vector2.up * this.JumpForce;
-                        if (this.body2d != null)
-                        {
-                            this.body2d.AddForce(jump);
-                        }
-                        else if (this.body != null)
-                        {
-                            this.body.AddForce(jump);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                this.jumpingTime -= Time.deltaTime;
-            }
+            //                Vector2 jump = Vector2.up * this.JumpForce;
+            //                if (this.body2d != null)
+            //                {
+            //                    this.body2d.AddForce(jump);
+            //                }
+            //                else if (this.body != null)
+            //                {
+            //                    this.body.AddForce(jump);
+            //                }
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        this.jumpingTime -= Time.deltaTime;
+            //    }
 
-            // 2d objects can't be moved in 3d "forward"
-            if (!this.isSprite)
-            {
-                if ((Input.GetAxisRaw("Vertical") < -0.1f) || (Input.GetAxisRaw("Vertical") > 0.1f))
-                {
-                    transform.position += Vector3.forward * (Speed * Time.deltaTime) * Input.GetAxisRaw("Vertical");
-                }
-            }
+            //    // 2d objects can't be moved in 3d "forward"
+            //    if (!this.isSprite)
+            //    {
+            //        if ((Input.GetAxisRaw("Vertical") < -0.1f) || (Input.GetAxisRaw("Vertical") > 0.1f))
+            //        {
+            //            transform.position += Vector3.forward * (Speed * Time.deltaTime) * Input.GetAxisRaw("Vertical");
+            //        }
+            //    }
+            //}
         }
     }
 }
